e959dec306736b9179d6581922e937ed6baffc54
M-tech-cmd <127322142+M-tech-cmd@users.noreply.github.com>
2025-09-05 13:03:19 +0300

    Update Firebase admin configuration and ignore sensitive files

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps} from "firebase/app";
+import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyD7FQwAzFZQ5x2hICJnq3cfk-Wawr05kYs",
@@ -12,7 +11,7 @@ const firebaseConfig = {
   messagingSenderId: "898437473677",
   appId: "1:898437473677:web:cbd1beff3fa0fffb4f89c4",
   measurementId: "G-8PXCQ8R9B6"
-}; 
+};
 
 // Initialize Firebase
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
